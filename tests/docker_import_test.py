6cb2b8ce66f1d071bbe85f56b3749301f0ec5302
dawit0905 <dawit0905@gmail.com>
2025-03-03 20:03:56 +0900

    add: reformatting files

--- tests/docker_import_test.py
+++ tests/docker_import_test.py
@@ -1,11 +1,13 @@
 import docker
 
+
 def test_docker_import():
     client = docker.DockerClient()  # 명시적 URL
     # 또는 client = docker.from_env()  # 환경 변수 기반
     version = client.version()["ApiVersion"]
     print(f"Docker API version: {version}")
     assert version is not None
+
 
 if __name__ == "__main__":
     test_docker_import()
